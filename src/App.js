a8e04b52b7597ca2185b38f33d7a9f5cc08c9c7c
MaxKorovchenko <max.korovchenko@gmail.com>
2023-08-01 18:12:29 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { Layout } from './components/Layout/Layout';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const ProjectsPage = lazy(() => import('./pages/ProjectsPage'));
+const ProjectDetailsPage = lazy(() => import('./pages/ProjectDetailsPage'));
+const ContactsPage = lazy(() => import('./pages/ContactsPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+
 function App() {
   return (
-    <div className="App">
-      <div>My portfolio</div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="projects" element={<ProjectsPage />} />
+        <Route path="projects/:id" element={<ProjectDetailsPage />} />
+        <Route path="contacts" element={<ContactsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
 }
 
