82926b1b5f9e7014b42f723b8bd8223a182751ad
MaxKorovchenko <max.korovchenko@gmail.com>
2023-11-04 10:54:38 +0200

    add NotFoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,16 @@
+import notFoundImage from '../assets/images/pages/NotFound.jpg';
+
 const NotFoundPage = () => {
-  return <>NotFoundPage</>;
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${notFoundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: `calc(100vh - 160px)`,
+      }}
+    ></div>
+  );
 };
 
 export default NotFoundPage;
