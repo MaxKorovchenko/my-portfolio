ac79a3969942b337b30adbd42e564b29b3e35705
MaxKorovchenko <max.korovchenko@gmail.com>
2023-11-05 14:35:28 +0200

    add download resume feature

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,14 @@
+import CV from 'assets/CV/Max_Korovchenko_Front-End_Developer.pdf';
+
 import styles from './Footer.module.css';
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div>Telegram GitHub LinkeId</div>
+      <a className={styles.resume} href={CV} download>
+        Download Resume
+      </a>
+
       <p>© 2023</p>
     </footer>
   );
