dc898d2961ef50c18094d7d3bdbb01aefffa0622
MaxKorovchenko <max.korovchenko@gmail.com>
2023-11-06 13:46:57 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { Layout } from 'components/Layout/Layout';
 
@@ -7,7 +7,7 @@ const HomePage = lazy(() => import('pages/HomePage'));
 const ProjectsPage = lazy(() => import('pages/ProjectsPage'));
 const ProjectDetailsPage = lazy(() => import('pages/ProjectDetailsPage'));
 const ContactsPage = lazy(() => import('pages/ContactsPage'));
-// const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
 
 export const App = () => {
   return (
@@ -17,7 +17,7 @@ export const App = () => {
         <Route path="projects" element={<ProjectsPage />} />
         <Route path="projects/:projectId" element={<ProjectDetailsPage />} />
         <Route path="contacts" element={<ContactsPage />} />
-        <Route path="*" element={<Navigate to="/" replace={true} />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
