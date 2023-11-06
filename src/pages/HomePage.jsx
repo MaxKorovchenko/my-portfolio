7b8131c921e89cd30a9ae93a4cb3bd829157af34
MaxKorovchenko <max.korovchenko@gmail.com>
2023-11-06 15:24:41 +0200

    add adaptive styles for home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,34 +3,13 @@ import { Skills } from 'components/Skills/Skills';
 import { AnimatedAbout } from 'utils/animations/AnimatedAbout';
 import { AnimatedSkills } from 'utils/animations/AnimatedSkills';
 
-const styles = {
-  container: {
-    width: 1240,
-    margin: '0 auto',
-    paddingLeft: 20,
-    paddingRight: 20,
-  },
-  title: {
-    color: 'aliceblue',
-    textAlign: 'center',
-    padding: 50,
-    fontSize: 50,
-  },
-  wrapper: {
-    display: 'flex',
-    justifyContent: 'center',
-    gap: 200,
-
-    marginTop: 'auto',
-    marginBottom: 'auto',
-  },
-};
+import styles from 'components/Layout/Layout.module.css';
 
 const HomePage = () => {
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>Hi! I'm Max, a Front-End Developer </h1>
-      <div style={styles.wrapper}>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Hi! I'm Max, a Front-End Developer </h1>
+      <div className={styles.wrapper}>
         <AnimatedAbout>
           <About />
         </AnimatedAbout>
